32977b59e759ac210e24354600eb3aca889530af
Ishan Anand <ishantechandothers@gmail.com>
2024-02-08 01:54:16 -0500

    Update gunicorn.conf.py
    Gunicorn debug code commented

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,9 +4,15 @@ import multiprocessing
 max_requests = 1000
 max_requests_jitter = 50
 
+# Capture Gunicorn logs to console
+#capture_output = True
+
 log_file = "-"
 
 bind = "0.0.0.0:3100"
-
+# Capture Gunicorn logs to console
+#capture_output = True
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = (multiprocessing.cpu_count() * 2) + 1
+# Set the log level to "info"
+#loglevel = "debug"
